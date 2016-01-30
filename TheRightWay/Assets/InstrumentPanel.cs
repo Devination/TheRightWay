2aac5baf262b558ce6ab01ce3c50d63265aad37c
Kelly MacNeill <kelly@pyromuffin>
2016-01-30 14:06:09 -0800

    removed event system

---
+++ TheRightWay/Assets/InstrumentPanel.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class InstrumentPanel : MonoBehaviour {
+
+	public Machine machine;
+	public GameObject dial;
+	public List<PanelButton> buttons;
+
+
+	public void Awake()
+	{
+		buttons = new List<PanelButton> ();
+
+	}
+
+	IEnumerator LerpDial()
+	{
+
+
+	}
+
+
+	public void NotifyHighlight( int index )
+	{
+
+
+	}
+
+
+	public void NotifyClick( int index )
+	{
+
+
+	}
+
+
+}
---
+++ TheRightWay/Assets/PanelButton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Linq;
+
+
+public class PanelButton : Button {
+
+	public override void OnPointerEnter (UnityEngine.EventSystems.PointerEventData eventData)
+	{
+		base.OnPointerEnter (eventData);
+
+		var panel = GetComponentInParent<InstrumentPanel> ();
+		int index = panel.buttons.IndexOf (this);
+		panel.NotifyHighlight (index);
+	}
+
+
+	public override void OnPointerClick (UnityEngine.EventSystems.PointerEventData eventData)
+	{
+		base.OnPointerClick (eventData);
+
+		var panel = GetComponentInParent<InstrumentPanel> ();
+		int index = panel.buttons.IndexOf (this);
+		panel.NotifyClick (index);
+	}
+
+}
