d4a2181f42dc0740658ff771834454310fe5cbbc
Kelly MacNeill <kelly@pyromuffin>
2016-01-30 15:55:16 -0800

    panels i hope this shit doesn't break or I'm gonna be mad

---
+++ TheRightWay/Assets/PanelArea.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PanelArea : MonoBehaviour {
+
+	public GameObject panel;
+
+	void OnTriggerEnter()
+	{
+		panel.SetActive (true);
+	}
+
+	void OnTriggerExit()
+	{
+		panel.SetActive (false);
+	}
+
+}
