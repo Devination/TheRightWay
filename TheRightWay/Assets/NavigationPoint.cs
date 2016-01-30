110fde7e47f3576a9750b757e8e60c27cd7bf44b
Kelly MacNeill <kelly@pyromuffin>
2016-01-30 10:18:03 -0800

    hope this doesn't ruin anything

---
+++ TheRightWay/Assets/NavigationPoint.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavigationPoint : MonoBehaviour {
+
+
+	void OnMouseDown()	{
+		PlayerNavigator.SetTarget (gameObject);
+	}
+
+}
---
+++ TheRightWay/Assets/PlayerNavigator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerNavigator : MonoBehaviour {
+
+	public static PlayerNavigator i;
+
+	public static void SetTarget( GameObject go )
+	{
+		i.NavigateToPoint( go.transform.position);
+	}
+
+	public NavMeshAgent navAgent;
+
+	void NavigateToPoint( Vector3 targetPos )
+	{
+		navAgent.destination = targetPos;
+	}
+
+	void Awake()
+	{
+		i = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
