3925fac61fd227218434467f0099035822c9bc57
Redbeard Duadikos <redbeard@duadikos.com>
2016-01-30 12:10:58 -0800

    adding herometer and hint text

---
+++ TheRightWay/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public Text endText;
+    public Slider slider;
+
+    void Awake () {
+        endText.text = "";
+
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (slider.value == 1) {
+            endGame();
+        }
+	}
+
+    public void endGame() {
+        endText.text = "Unacceptable!";
+    }
+
+    void updateText() {
+        
+    }
+}
