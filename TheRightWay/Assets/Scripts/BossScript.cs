eb88c3b43d2649aa63f441006e33b735cc35c7df
Redbeard Duadikos <redbeard@duadikos.com>
2016-01-30 14:55:20 -0800

    boss script

---
+++ TheRightWay/Assets/Scripts/BossScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BossScript : MonoBehaviour {
+
+    public Text bossText;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void bossSays(string words) {
+        bossText.text = words;
+    }
+}
