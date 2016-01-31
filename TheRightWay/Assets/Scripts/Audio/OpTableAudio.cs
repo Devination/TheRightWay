c94b7a7e5e9a5a2b42607f688ac73cab7811b210
Devin Curry - Laptop <Devination3D@gmail.com>
2016-01-30 19:13:49 -0800

    Add Laser Sound to operation table

--- TheRightWay/Assets/Scripts/Audio/OpTableAudio.cs
+++ TheRightWay/Assets/Scripts/Audio/OpTableAudio.cs
@@ -2,10 +2,10 @@
 using System.Collections;
 
 public class OpTableAudio : MonoBehaviour {
-
+	AudioSource _audio;
 	// Use this for initialization
 	void Start () {
-	
+		_audio = this.GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -15,11 +15,19 @@ public class OpTableAudio : MonoBehaviour {
     
     public AudioClip laserLoop;
     public void playLaserLoop() {
-
+		_audio.clip = laserLoop;
+		_audio.loop = true;
+		_audio.Play();
     }
 
     public AudioClip laserStart;
     public void playLaserStart() {
-
-    }
+		_audio.clip = laserStart;
+		_audio.loop = false;
+		_audio.Play();
+	}
+	public void StopSound()
+	{
+		_audio.Stop();
+	}
 }
