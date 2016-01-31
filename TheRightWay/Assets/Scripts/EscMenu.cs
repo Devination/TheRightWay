9d9712690ec1c4dcf5d8d9b15da80395a4ad9ff3
Chris Lai <hellphish@gmail.com>
2016-01-30 21:08:33 -0800

    adding menu and Esc Script

---
+++ TheRightWay/Assets/Scripts/EscMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class EscMenu : MonoBehaviour {
+
+		
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(0);
+        }
+	}
+}
