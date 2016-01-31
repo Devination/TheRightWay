6f250ed0fa38b78e6cdc5485a5bb7c9b987bfd3b
Kelly MacNeill <kelly@pyromuffin>
2016-01-30 19:00:03 -0800

    machine strings and some meta files!

--- TheRightWay/Assets/Machine.cs
+++ TheRightWay/Assets/Machine.cs
@@ -1,19 +1,71 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
+public struct MachineStrings
+{
+	string[] hint;
+	string[] feedbackGood;
+	string[] feedbackBad;
+	string[] endText;
+}
+
+
+public enum WhichMachine
+{
+	Boiler, HatSharpener
+};
+
 public class Machine : MonoBehaviour {
 
-	public string[] states;
 	public string[] hints;
+	public string[] feedbackGood;
+	public string[] feedbackBad;
+	public string[] endText;
+	public string[] states;
 	public string[] animatorStateNames;
-    public string[] endText;
 	public int currentState;
 	public int correctState;
 	public Animator animator;
+	public WhichMachine whichMachine;
+
+	/*
+	public MachineStrings GetStrings()
+	{
+		switch( whichMachine) 
+		return 
+
+	}
+	*/
 
 
 	public void ChangeState( int index )
 	{
 		animator.SetTrigger (animatorStateNames [index]);
 	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// begin machine strings
+	/*
+	public static MachineStrings BoilerStrings
+	{
+		{  "a", "b", "c"
+
+
+	}
+		*/
+
 }
--- TheRightWay/Assets/NavigationPoint.cs
+++ TheRightWay/Assets/NavigationPoint.cs
@@ -1,11 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.EventSystems;
 
 public class NavigationPoint : MonoBehaviour {
 
 	public GameObject navObject;
 
 	void OnMouseDown()	{
+		bool overUI = EventSystem.current.IsPointerOverGameObject ();
+		if (overUI)
+			return;
+
 		PlayerNavigator.SetTarget (navObject);
 	}
 
