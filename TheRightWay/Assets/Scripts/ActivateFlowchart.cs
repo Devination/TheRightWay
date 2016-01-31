aebb77ae92270133a09ba22009cd4de7fc58d483
Chris Lai <hellphish@gmail.com>
2016-01-30 21:53:37 -0800

    Main manu, options menu, opening cutscene.

---
+++ TheRightWay/Assets/Scripts/ActivateFlowchart.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateFlowchart : MonoBehaviour {
+
+    public GameObject flowchart;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	public void turnOnFlowchart ()
+    {
+        flowchart.SetActive(true);
+	}
+}
